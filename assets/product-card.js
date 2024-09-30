38c8fe6655382f13401ef97d9643a9ad45b1a29a
miske-marvya <me@marvya.de>
2024-09-30 12:55:15 +0800

    refactor code

--- assets/product-card.js
+++ assets/product-card.js
@@ -11,68 +11,80 @@ if (!customElements.get('product-card')) {
       handleQuickAddButton() {
         const QuickAddButton = this.querySelector('.quick-add__btn');
         const loadingSpinner = QuickAddButton.querySelector('.loading__spinner');
-        QuickAddButton.addEventListener('click', () => {
-          const productUrl = `/products/${this.product.data.handle}`;
-          QuickAddButton.classList.add('loading');
-          loadingSpinner.classList.remove('hidden');
-          fetch(productUrl)
-            .then((response) => response.text())
-            .then((responseText) => {
-              const html = new DOMParser().parseFromString(responseText, 'text/html');
-              const template = html.querySelector(`#drawer-product-${this.product.data.id}`);
-              if (template) {
-                document.body.appendChild(template.content);
-                const drawer = document.querySelector('.drawer-product-selector');
-                requestAnimationFrame(() => {
-                  drawer.classList.add('show-product-selector');
-                });
-                this.handleDrawerClose(drawer);
-                this.drawer = drawer;
-                this.onVariantChangeUnsubscriber = subscribe(
-                  PUB_SUB_EVENTS.optionValueCardSelectionChange,
-                  this.handleOptionValueChange.bind(this)
-                );
-              }
-            })
-            .then(() => {})
-            .catch((error) => {
-              if (error.name === 'AbortError') {
-                console.log('Fetch aborted by user');
-              } else {
-                console.error(error);
-              }
-            })
-            .finally(() => {
-              QuickAddButton.classList.remove('loading');
-              loadingSpinner.classList.add('hidden');
-            });
+        QuickAddButton.addEventListener('click', () => this.handleQuickAddClick(QuickAddButton, loadingSpinner));
+      }
+
+      async handleQuickAddClick(QuickAddButton, loadingSpinner) {
+        const productUrl = `/products/${this.product.data.handle}`;
+        this.toggleLoadingState(QuickAddButton, loadingSpinner, true);
+
+        try {
+          const responseText = await this.fetchProductData(productUrl);
+          const template = this.extractTemplate(responseText);
+
+          if (template) {
+            this.appendTemplateContent(template);
+            this.initializeDrawer();
+          }
+        } catch (error) {
+          this.handleFetchError(error);
+        } finally {
+          this.toggleLoadingState(QuickAddButton, loadingSpinner, false);
+        }
+      }
+
+      async fetchProductData(url) {
+        const response = await fetch(url);
+        return response.text();
+      }
+
+      extractTemplate(responseText) {
+        const html = new DOMParser().parseFromString(responseText, 'text/html');
+        return html.querySelector(`#drawer-product-${this.product.data.id}`);
+      }
+
+      appendTemplateContent(template) {
+        document.body.appendChild(template.content);
+      }
+
+      initializeDrawer() {
+        const drawer = document.querySelector('.drawer-product-selector');
+        requestAnimationFrame(() => {
+          drawer.classList.add('show-product-selector');
         });
+        this.handleDrawerClose(drawer);
+        this.drawer = drawer;
+        this.onVariantChangeUnsubscriber = subscribe(
+          PUB_SUB_EVENTS.optionValueCardSelectionChange,
+          this.handleOptionValueChange.bind(this)
+        );
       }
 
       handleDrawerClose(drawer) {
         const overlayDrawer = drawer.querySelector('.drawer-product-selector__overlay');
         const xIcon = drawer.querySelector('.close-icon-drawer');
         [overlayDrawer, xIcon].forEach((close) => {
-          close.addEventListener('click', () => {
-            // Remove the class that triggers the transition
-            drawer.classList.remove('show-product-selector');
-
-            setTimeout(() => {
-              drawer.remove();
-            }, 800);
-          });
+          close.addEventListener('click', () => this.closeDrawer(drawer));
         });
       }
 
-      buildRequestUrlWithParams(url, optionValues) {
-        const params = [];
+      closeDrawer(drawer) {
+        drawer.classList.remove('show-product-selector');
+        setTimeout(() => drawer.remove(), 800);
+      }
 
-        // params.push(`sections=template--23234586476879__main`);
-
-        if (optionValues.length) {
-          params.push(`option_values=${optionValues.join(',')}`);
+      toggleLoadingState(button, spinner, isLoading) {
+        if (isLoading) {
+          button.classList.add('loading');
+          spinner.classList.remove('hidden');
+        } else {
+          button.classList.remove('loading');
+          spinner.classList.add('hidden');
         }
+      }
 
+      buildRequestUrlWithParams(url, optionValues) {
+        const params = optionValues.length ? [`option_values=${optionValues.join(',')}`] : [];
         return `${url}?${params.join('&')}`;
       }
 
@@ -84,48 +96,64 @@ if (!customElements.get('product-card')) {
 
       getSelectedVariant(productInfoNode) {
         const selectedVariant = productInfoNode.querySelector('variant-selects [data-selected-variant]')?.innerHTML;
-        return !!selectedVariant ? JSON.parse(selectedVariant) : null;
+        return selectedVariant ? JSON.parse(selectedVariant) : null;
       }
 
-      handleOptionValueChange({ data: { event, target, selectedOptionValues, privateEventId } }) {
+      handleOptionValueChange({ data: { privateEventId, selectedOptionValues, target } }) {
         if (privateEventId === `product-card-${this.product.data.id}`) {
-          const productUrl = `/products/${this.product.data.handle}`;
-          this.resetProductFormState();
-          const requestUrl = this.buildRequestUrlWithParams(productUrl, selectedOptionValues);
-          this.abortController = new AbortController();
+          this.updateVariant(selectedOptionValues, target);
+        }
+      }
 
-          fetch(requestUrl, { signal: this.abortController.signal })
-            .then((response) => response.text())
-            .then((responseText) => {
-              this.pendingRequestUrl = null;
-              const html = new DOMParser().parseFromString(responseText, 'text/html');
-              const template = html.querySelector(`#drawer-product-${this.product.data.id}`);
-              const newDrawer = template.content.querySelector('.drawer-product-selector');
-              const variant = this.getSelectedVariant(newDrawer);
-              this.drawer.querySelector('variant-selects').innerHTML =
-                newDrawer.querySelector('variant-selects').innerHTML;
-              this.drawer.querySelector('.drawer__header-product-price').innerHTML = newDrawer.querySelector(
-                '.drawer__header-product-price'
-              ).innerHTML;
-              this.drawer.querySelector('.product-variant-id').value = variant.id;
-              this.drawer
-                .querySelector('product-form')
-                .toggleSubmitButton(
-                  newDrawer.querySelector(`.product-form__submit`)?.hasAttribute('disabled') ?? true,
-                  window.variantStrings.soldOut
-                );
-            })
-            .then(() => {
-              // set focus to last clicked option value
-              this.querySelector(`#${target.id}`)?.focus();
-            })
-            .catch((error) => {
-              if (error.name === 'AbortError') {
-                console.log('Fetch aborted by user');
-              } else {
-                console.error(error);
-              }
-            });
+      async updateVariant(selectedOptionValues, target) {
+        const productUrl = `/products/${this.product.data.handle}`;
+        this.resetProductFormState();
+        const requestUrl = this.buildRequestUrlWithParams(productUrl, selectedOptionValues);
+        this.abortController = new AbortController();
+
+        try {
+          const responseText = await this.fetchVariantData(requestUrl);
+          this.updateDrawerContent(responseText);
+          this.focusOnTarget(target);
+        } catch (error) {
+          this.handleFetchError(error);
+        }
+      }
+
+      async fetchVariantData(url) {
+        const response = await fetch(url, { signal: this.abortController.signal });
+        return response.text();
+      }
+
+      updateDrawerContent(responseText) {
+        const html = new DOMParser().parseFromString(responseText, 'text/html');
+        const template = html.querySelector(`#drawer-product-${this.product.data.id}`);
+        const newDrawer = template.content.querySelector('.drawer-product-selector');
+        const variant = this.getSelectedVariant(newDrawer);
+
+        this.drawer.querySelector('variant-selects').innerHTML = newDrawer.querySelector('variant-selects').innerHTML;
+        this.drawer.querySelector('.drawer__header-product-price').innerHTML = newDrawer.querySelector(
+          '.drawer__header-product-price'
+        ).innerHTML;
+        this.drawer.querySelector('.product-variant-id').value = variant.id;
+
+        this.drawer
+          .querySelector('product-form')
+          .toggleSubmitButton(
+            newDrawer.querySelector(`.product-form__submit`)?.hasAttribute('disabled') ?? true,
+            window.variantStrings.soldOut
+          );
+      }
+
+      focusOnTarget(target) {
+        this.querySelector(`#${target.id}`)?.focus();
+      }
+
+      handleFetchError(error) {
+        if (error.name === 'AbortError') {
+          console.log('Fetch aborted by user');
+        } else {
+          console.error(error);
         }
       }
     }
